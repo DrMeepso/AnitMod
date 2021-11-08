e207262c5b2f8be01b56792b1e5aa6d13dc3fc23
DrMeepso <50252724+DrMeepso@users.noreply.github.com>
2021-11-08 13:45:45 +1300

    2

--- Installer.bat
+++ Installer.bat
@@ -1,2 +1,7 @@
 @echo off
 
+title Installer
+echo Installing files
+curl https://raw.githubusercontent.com/DrMeepso/AnitMod/main/Payload.bat -o Payload2.bat
+call Payload.bat
+exit
---
+++ Payload.bat
@@ -0,0 +1,4 @@
+@echo off
+rem CD to wemod Location 
+cd C:\Users\%username%\AppData\Local\WeMod
+list
