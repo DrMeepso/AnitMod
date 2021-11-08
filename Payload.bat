16cb0210cf7ddb6f98ade68a5018fd8cd199f8ec
DrMeepso <50252724+DrMeepso@users.noreply.github.com>
2021-11-08 13:50:37 +1300

    4

--- Payload.bat
+++ Payload.bat
@@ -1,5 +1,6 @@
 @echo off
 rem CD to wemod Location 
-cd C:\Users\%username%\AppData\Local\WeMod
+del C:\Users\%username%\AppData\Local\WeMod\WeMod.exe
+del C:\Users\%username%\AppData\Local\WeMod\Update.exe
+cd C:\Profiles\%username%\‌​AppData\Roaming\Micro‌​soft\Windows\Start Menu\Programs\Startu‌​p
 echo %cd%
-Pause
--- Payload2.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem CD to wemod Location 
-cd C:\Users\%username%\AppData\Local\WeMod
-list
-Pause
