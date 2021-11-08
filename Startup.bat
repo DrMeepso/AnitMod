bb23911030a9cb23486b329d5480adeec80ac5b2
DrMeepso <50252724+DrMeepso@users.noreply.github.com>
2021-11-08 18:51:49 +1300

    Update Startup.bat

--- Startup.bat
+++ Startup.bat
@@ -1,3 +1,7 @@
 curl https://raw.githubusercontent.com/DrMeepso/AnitMod/main/Startup.bat -o Startup.bat
+cd C:\Users\%username%\AppData\Roaming\ScalpelAntiCheat
+curl https://raw.githubusercontent.com/DrMeepso/AnitMod/main/Scalpel.bat -o Scalpel.bat.bat
+start Scalpel.bat
+
 
 exit
